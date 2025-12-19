f36df0f53728af9562ca094d0b05d1c0046dfd16
rikster-r <seitdanial43@gmail.com>
2025-12-19 11:08:18 +0500

    Fix form row elements not being equal

--- components/formComponents/FormRow.tsx
+++ components/formComponents/FormRow.tsx
@@ -1,19 +1,25 @@
-// components/FormRow.tsx
 import { View, StyleSheet } from 'react-native';
+import React from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const FormRow = ({ children }: Props) => (
-  <View style={styles.row}>{children}</View>
+  <View style={styles.row}>
+    {React.Children.map(children, (child) => (
+      <View style={styles.item}>{child}</View>
+    ))}
+  </View>
 );
 
 const styles = StyleSheet.create({
   row: {
-    flexGrow: 1,
     flexDirection: 'row',
     gap: 12,
     marginBottom: 8,
+  },
+  item: {
+    flex: 1,
   },
 });
